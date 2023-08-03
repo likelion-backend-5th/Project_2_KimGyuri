dae1c3e1898eb99c5e1e12d8cd2cc9748fe93da0
kkyuwoo <ggya5@naver.com>
2023-08-03 15:26:16 +0900

    feat: JwtRequestDto, JwtTokenDto 추가 - 토큰 생성 및 발급 (로그인) 시 필요한 데이터 받고 반환

---
+++ src/main/java/com/example/Project_2_KimGyuri/jwt/dto/JwtRequestDto.java
@@ -0,0 +1,9 @@
+package com.example.Project_2_KimGyuri.jwt.dto;
+
+import lombok.Data;
+
+@Data
+public class JwtRequestDto {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/example/Project_2_KimGyuri/jwt/dto/JwtTokenDto.java
@@ -0,0 +1,8 @@
+package com.example.Project_2_KimGyuri.jwt.dto;
+
+import lombok.Data;
+
+@Data
+public class JwtTokenDto {
+    private String token;
+}
