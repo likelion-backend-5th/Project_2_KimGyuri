a81916461b10c9238565de546da3c4fc2ec5335f
kkyuwoo <ggya5@naver.com>
2023-08-03 11:22:06 +0900

    First Commit

---
+++ src/main/java/com/example/Project_2_KimGyuri/Project2KimGyuriApplication.java
@@ -0,0 +1,13 @@
+package com.example.Project_2_KimGyuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2KimGyuriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2KimGyuriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Project_2_KimGyuri/Project2KimGyuriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Project_2_KimGyuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2KimGyuriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
