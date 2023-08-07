103cf171dcd927bf7884491b297d95a1756fc0b6
kkyuwoo <ggya5@naver.com>
2023-08-07 10:28:26 +0900

    feat: 피드 단독 조회 기능에 댓글 조회 추가

--- src/main/java/com/example/Project_2_KimGyuri/dto/CommentDto.java
+++ src/main/java/com/example/Project_2_KimGyuri/dto/CommentDto.java
@@ -6,12 +6,15 @@ import lombok.Data;
 
 @Data
 public class CommentDto {
+    private String username;
+
     @NotBlank(message = "내용을 입력해주세요.")
     private String content;
 
     public static CommentDto fromEntity(CommentEntity entity) {
         CommentDto dto = new CommentDto();
-        dto.setContent(dto.getContent());
+        dto.setUsername(entity.getUsersId().getUsername());
+        dto.setContent(entity.getContent());
         return dto;
     }
 }
--- src/main/java/com/example/Project_2_KimGyuri/dto/OneArticleDto.java
+++ src/main/java/com/example/Project_2_KimGyuri/dto/OneArticleDto.java
@@ -2,11 +2,11 @@ package com.example.Project_2_KimGyuri.dto;
 
 import com.example.Project_2_KimGyuri.entity.ArticleEntity;
 import com.example.Project_2_KimGyuri.entity.ArticleImagesEntity;
+import com.example.Project_2_KimGyuri.entity.CommentEntity;
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Data
 public class OneArticleDto {
@@ -14,17 +14,29 @@ public class OneArticleDto {
     private String title;
     private String content;
     private List<String> imageUrls;
+    private List<CommentDto> comments;
 
     public static OneArticleDto fromEntity(ArticleEntity entity) {
         OneArticleDto dto = new OneArticleDto();
         dto.setUsername(entity.getUsersId().getUsername());
         dto.setTitle(entity.getTitle());
         dto.setContent(entity.getContent());
+
         List<String> imageUrls = new ArrayList<>();
         for (ArticleImagesEntity image : entity.getArticleImages()) {
             imageUrls.add(image.getImageUrl());
         }
         dto.setImageUrls(imageUrls);
+
+        List<CommentDto> comments = new ArrayList<>();
+        for (CommentEntity comment : entity.getComments()) {
+            CommentDto commentDto = new CommentDto();
+            commentDto.setUsername(comment.getUsersId().getUsername());
+            commentDto.setContent(comment.getContent());
+            comments.add(commentDto);
+        }
+        dto.setComments(comments);
+
 
         return dto;
     }
